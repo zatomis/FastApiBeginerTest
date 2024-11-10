362fd82df84045e020e4621903782413a128a559
zatomis <zatomis@Boron3070>
2024-11-10 15:46:08 +0300

    Ускоренная документация. Переопределение функций

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
 from fastapi import FastAPI
+from fastapi.openapi.docs import get_swagger_ui_html
 import uvicorn
 
+app = FastAPI(docs_url=None)
 
-app = FastAPI()
 
 @app.get("/")
 def func():
-    return "Hello"
+    return "Hello World!!!!!!!!!!"
+
+
+@app.get("/docs", include_in_schema=False)
+async def custom_swagger_ui_html():
+    return get_swagger_ui_html(
+        openapi_url=app.openapi_url,
+        title=app.title + " - Swagger UI",
+        oauth2_redirect_url=app.swagger_ui_oauth2_redirect_url,
+        swagger_js_url="https://unpkg.com/swagger-ui-dist@5/swagger-ui-bundle.js",
+        swagger_css_url="https://unpkg.com/swagger-ui-dist@5/swagger-ui.css",
+    )
+
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
