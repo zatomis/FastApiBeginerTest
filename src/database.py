092537256896deb3010d1a2ccf76190f011b77d8
zatomis <zatomis@list.ru>
2024-11-24 19:30:44 +0300

    БД. Пример сырого запроса из бд

--- src/database.py
+++ src/database.py
@@ -1,3 +1,15 @@
-from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
+import asyncio
 
-engine = create_async_engine()
+from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
+from sqlalchemy import text
+
+from src.config import settings
+
+engine = create_async_engine(settings.DB_URL)
+
+async def func():
+    async with engine.begin() as conn:
+        res = await conn.execute(text("SELECT version()"))
+        print(res.fetchone())
+
+asyncio.run(func())
--- src/main.py
+++ src/main.py
@@ -9,9 +9,7 @@ from pathlib import Path
 sys.path.append(str(Path(__file__).parent.parent))
 
 from src.api.hotels import router as router_hotels
-from src.config import settings
-
-print(f"{settings.DB_URL=}")
+from src.database import *
 
 
 app = FastAPI(docs_url=None)
