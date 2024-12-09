8a42f32f37d946b5a26365086259d4f82b77e67a
zatomis <zatomis@list.ru>
2024-12-09 11:31:02 +0300

    Сервис авторизации в отдельном классе

--- src/api/auth.py
+++ src/api/auth.py
@@ -1,36 +1,17 @@
-from datetime import datetime, timezone, timedelta
-from passlib.context import CryptContext
 from fastapi import APIRouter, HTTPException, Response
-
-from src.config import settings
 from src.database import new_async_session_maker
 from src.repositories.users import UsersRepository
 from src.schemas.users import UserRequestAdd, UserAdd
-
-import jwt
+from src.services.auth import AuthService
 
 router = APIRouter(prefix="/auth", tags=["Авторизация и аутентификация"])
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
-
-def create_access_token(data: dict) -> str:
-    to_encode = data.copy()
-    expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)#время жизни токена
-    to_encode.update({"exp": expire})
-    encoded_jwt = jwt.encode(to_encode, settings.JWT_SECRET_KEY, algorithm=settings.JWT_ALGORITHM)
-    return encoded_jwt
-
-
-def verify_password(plain_password, hashed_password):
-    return pwd_context.verify(plain_password, hashed_password)
 
 
 @router.post("/register")
 async def register_user(
         data: UserRequestAdd,
 ):
-    hashed_passwd = pwd_context.hash(data.password)
+    hashed_passwd = AuthService().hash_password(data.password)
 
     async with (new_async_session_maker() as session):
         email = await UsersRepository(session).get_one_or_none(email=data.email)
@@ -58,9 +39,9 @@ async def login_user(
             raise HTTPException(status_code=401, detail=f"Нет такого пользователя с {data.email} ! ")
 
         #проверим пароль юзера
-        if not verify_password(data.password, user.password):
+        if not AuthService().verify_password(data.password, user.password):
             raise HTTPException(status_code=401, detail=f"Пароль не верный ! ")
 
-        access_token = create_access_token({"user_id": user.id})
+        access_token = AuthService().create_access_token({"user_id": user.id})
         response.set_cookie("access_token", access_token)
         return {"access_token": access_token}
