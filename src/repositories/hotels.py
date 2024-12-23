f0cdd107728e68d519d3a03b51e24417f93b00e7
zatomis <zatomis@list.ru>
2024-12-23 15:43:45 +0300

    Получение свободных номеров и отелей за период

--- src/repositories/hotels.py
+++ src/repositories/hotels.py
@@ -22,10 +22,12 @@ class HotelRepository(BaseRepository):
     )-> list[Hotel]:
 
         query_hotel_statement = select(HotelsORM)
+
         if location:
             query_hotel_statement = query_hotel_statement.where(func.lower(HotelsORM.location).like(f'%{location.strip().lower()}%'))
         if title:
             query_hotel_statement = query_hotel_statement.where(func.lower(HotelsORM.title).like(f'%{title.strip().lower()}%'))
+
         query_hotel_statement = (
             query_hotel_statement
             .limit(limit)
@@ -37,13 +39,19 @@ class HotelRepository(BaseRepository):
 
 
     async def get_filter_by_time(self,
+                                 location,
+                                 title,
+                                 limit,
+                                 offset,
                                  date_from: date,
                                  date_to: date):
+
         rooms_ids_to_get = rooms_ids_for_booking(date_from=date_from, date_to=date_to)
         hotels_ids = (
             select(RoomsORM.hotel_id)
             .select_from(RoomsORM)
             .filter(RoomsORM.id.in_(rooms_ids_to_get))
         )
+
         return await self.get_filter(HotelsORM.id.in_(hotels_ids))
 
