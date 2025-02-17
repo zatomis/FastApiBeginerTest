5661680934166326f35e77452f462dc78b226237
zatomis <zatomis@list.ru>
2025-02-17 14:57:01 +0300

    Тестирование с параметрами для бронирования

--- tests/integration_tests/bookings/test_api.py
+++ tests/integration_tests/bookings/test_api.py
@@ -1,12 +1,17 @@
+from pprint import pprint
+
 import pytest
+
+from src.database import new_async_session_maker_null_pool
+from src.utils.db_manager import DBManager
+
 
 @pytest.mark.parametrize("room_id, date_from, date_to, status_code",[
     (1, "2024-02-01", "2024-02-11", 200),
     (1, "2024-02-01", "2024-02-11", 200),
+    (2, "2024-02-01", "2024-02-11", 200),
     (1, "2024-02-01", "2024-02-11", 200),
     (1, "2024-02-01", "2024-02-11", 200),
-    (1, "2024-02-01", "2024-02-11", 200),
-    (1, "2024-02-01", "2024-02-11", 500),
 ])
 async def test_add_booking(
         room_id, date_from, date_to, status_code,
@@ -26,3 +31,33 @@ async def test_add_booking(
         assert isinstance(res, dict)
         assert res["status"] == "OK"
         assert "data" in res
+
+
+
+@pytest.fixture(scope="session")
+async def clear_DB_bookings(check_test_mode):
+    print("ФИКСТУРА на очистку бронирований")
+    async with DBManager(session_factory=new_async_session_maker_null_pool) as db_:
+        await db_.bookings.remove()
+        await db_.commit()
+
+
+
+@pytest.mark.parametrize("room_id, date_from, date_to, status_code",[
+    (1, "2024-02-01", "2024-02-03", 1),
+    (1, "2024-02-01", "2024-02-03", 2),
+    (1, "2024-02-01", "2024-02-03", 3),])
+async def test_add_and_get_my_bookings(
+    clear_DB_bookings, db,
+    room_id, date_from, date_to, status_code,
+    autheticated_user_ac
+    ):
+    user = (await db.users.get_all())[0]
+
+    response = await autheticated_user_ac.get(
+        "/bookings/me",
+        params={
+            "user_id": user.id,
+        }
+    )
+    pprint(f"{response.json()=}")
